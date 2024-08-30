426e4e1b335d1a9ac54eb04954276d181e33423f
Hungvu <hungvm120402@gmail.com>
2024-08-30 21:59:51 +0700

    Booking API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoomModule } from './room/room.module';
+import { BookingModule } from './booking/booking.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RoomModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), BookingModule, RoomModule, PrismaModule],
 })
 export class AppModule {}
