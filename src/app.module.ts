5d4b6840090665106ef7d58ec19b946f54c8df34
Hungvu <hungvm120402@gmail.com>
2024-08-30 21:57:09 +0700

    Room API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [],
+  imports: [RoomModule, PrismaModule],
 })
 export class AppModule {}
