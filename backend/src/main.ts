f08df7c81cfd01e8647a374e2a01f87af570c11d
Hungvu <hungvm120402@gmail.com>
2024-08-31 00:01:52 +0700

    vercel deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { Handler } from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const server = express();
+
+export const createNestServer = async (
+  expressInstance: any,
+): Promise<Handler> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
   app.enableCors();
-  await app.listen(3000);
-}
-bootstrap();
+  await app.init();
+  return expressInstance;
+};
+
+createNestServer(server);
+export default server;
