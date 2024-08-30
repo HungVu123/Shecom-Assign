f25759b67d15c17e9fbe2b13819c8ec63f6252b8
Hungvu <hungvm120402@gmail.com>
2024-08-31 01:38:05 +0700

    fix deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.init();
-  return server;
+  await app.listen(3000);
 }
-
-const serverPromise = bootstrap();
-
-export async function handler(req, res) {
-  const server = await serverPromise;
-  server(req, res);
-}
+bootstrap();
